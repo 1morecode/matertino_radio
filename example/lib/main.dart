aa51d398ea05c2c802d774fc17ffea9bdc7fa884
1morecode <1morecode@gmail.com>
2023-05-28 12:44:48 +0530

    User change

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Radio Checkbox',
+      title: 'Custom Radio Checkbox',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
